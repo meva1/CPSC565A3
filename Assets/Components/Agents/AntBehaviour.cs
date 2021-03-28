9c1ca87cd4ea383948c1cfc2dfc444e73517fe82
Murray Evans <meva@ucalgary.ca>
2021-03-27 19:54:54 -0600

    First commit, generate ants that do nothing but lose health

---
+++ Assets/Components/Agents/AntBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntBehaviour : MonoBehaviour
+{
+
+    public int health;
+
+    private void Awake()
+    {
+
+        health = 1000;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Health();
+        
+    }
+
+    void Health()
+    {
+        health--;
+        if (health == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
